d36a3c4f3b89ceea66ca2c3ace5eeeb7f0207ff2
Rupam Shil <rupamshil111@gmail.com>
2025-05-06 23:21:13 +0530

    Add 3D sphere background with Three.js and React Three Fiber

--- features/home/components/home-second-section.tsx
+++ features/home/components/home-second-section.tsx
@@ -1,6 +1,7 @@
 import { MotionValue, useTransform, motion, useScroll } from 'framer-motion';
 import React, { useRef } from 'react';
 import PoolCollection from './pools/pool-collection';
+import SphereBgWrapper from './bg-wrapper/sphere-bg-wrapper';
 
 function HomeSecondSection({
 	scrollYProgress,
@@ -8,7 +9,7 @@ function HomeSecondSection({
 	scrollYProgress: MotionValue;
 }) {
 	const scale = useTransform(scrollYProgress, [0, 1], [0.96, 1]);
-	const y = useTransform(scrollYProgress, [0, 1], [-70, 0]);
+	const y = useTransform(scrollYProgress, [0, 1], [-90, 0]);
 	const borderRadius = useTransform(scrollYProgress, [0, 1], [10, 10]);
 	const padding = useTransform(scrollYProgress, [0, 1], [0, 5]);
 
@@ -30,7 +31,8 @@ function HomeSecondSection({
 			className='h-[300vh] w-screen relative z-2'>
 			<motion.div
 				style={{ borderRadius }}
-				className='w-full h-[calc(100vh-10px)] sticky top-[5px]  overflow-hidden'>
+				className='w-full h-[calc(100vh-10px)] sticky top-[5px]  overflow-hidden isolate'>
+				<SphereBgWrapper />
 				<PoolCollection
 					scrollYProgressCenter={containerScrollYProgressCenter}
 					scrollYProgressEnd={containerScrollYProgressEnd}
